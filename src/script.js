4b6332c3fbffa0eaaed62dfdaa256b72a2af3604
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-08-27 01:28:22 +0530

    add/ features, continue/building . working on the js. pending... the news feed.

--- src/script.js
+++ src/script.js
@@ -83,3 +83,73 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
     NewsBox.style.backgroundImage = `url(${data.url})`;
   }
 })
+
+
+
+
+// clock logic-->
+
+function clock(){
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  document.getElementById('hours').textContent = hours;
+  document.getElementById('minutes').textContent = minutes;
+};
+
+
+clock();
+setInterval(clock, 1000);
+
+
+// fetch News
+
+// news functions
+
+const FEEDS = [
+    'https://news.mit.edu/rss/feed',
+    'https://news.mit.edu/rss/topic/science',
+    'https://news.mit.edu/rss/topic/technology',
+    'https://news.mit.edu/rss/topic/artificial-intelligence2'
+];
+
+// defining functions
+
+function hash_news(feedUrl) {
+  let hash = 0;
+  for(let i=0; i<feedUrl.length; i++){
+    const char = feedUrl.charCodeAt(i);
+    hash = (hash<<5)-hash + char;
+    hash |= 0; 
+  }
+  return Math.abs(hash);
+}
+
+function CheakLocalStorage(hash){
+  const output = localStorage.getItem(hash);
+  return output !== null;
+}
+function FetchLocalStorage(hash){
+  const data = localStorage.getItem(hash);
+  return data;
+}
+function SaveLocalStorage(hash, data){
+  localStorage.setItem(hash, data);
+}
+
+function loadNews(){
+
+
+
+}
+
+function cheakDB(){
+  const today = new Date();
+  const date = today.getDate();
+  if(localStorage.getItem('Date') !== String(date)){
+    localStorage.clear();
+    localStorage.setItem('Date', String(date));
+    loadNews();
+  }
+  return 1;
+}
